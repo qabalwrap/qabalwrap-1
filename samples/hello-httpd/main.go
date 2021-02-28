c29134ea2b76f4cfe742bb3b3e6317deb17fe1b1
Cheyin L <cheyinl@gmail.com>
2021-03-01 03:08:33 +0800

    add basic http service for testing

---
+++ samples/hello-httpd/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"net/http"
+	"time"
+
+	utilhttphandlers "github.com/yinyin/go-util-http-handlers"
+)
+
+type helloHTTPResponse struct {
+	MessageText string
+	StartAt     time.Time
+	Host        string
+	Headers     map[string][]string
+}
+
+type helloHTTPHandler struct {
+	messageText string
+	startAt     time.Time
+}
+
+func (hnd *helloHTTPHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	utilhttphandlers.JSONResponseWithStatusOK(w, &helloHTTPResponse{
+		MessageText: hnd.messageText,
+		StartAt:     hnd.startAt,
+		Host:        r.Host,
+		Headers:     r.Header,
+	})
+}
+
+func parseCommandParam() (listenAt string, messageText string, err error) {
+	flag.StringVar(&listenAt, "listen", ":8080", "address:port to listen")
+	flag.StringVar(&messageText, "message", "Hello", "message to show in response")
+	flag.Parse()
+	if "" == listenAt {
+		return "", "", errors.New("require option `listen` is empty")
+	}
+	if "" == messageText {
+		return "", "", errors.New("require option `message` is empty")
+	}
+	return
+}
+
+func main() {
+	listenAt, messageText, err := parseCommandParam()
+	if nil != err {
+		log.Fatalf("invalid command option for hello-httpd: %v", err)
+		return
+	}
+	hnd := &helloHTTPHandler{
+		messageText: messageText,
+		startAt:     time.Now(),
+	}
+	log.Print("INFO: start.")
+	if err = http.ListenAndServe(listenAt, hnd); nil != err {
+		log.Printf("WARN: http.ListenAndServe return no nil result: %v", err)
+	}
+	log.Print("INFO: stopped.")
+}
