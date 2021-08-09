c311c3804b4524ba89785799463a2470d1da3fbb
Cheyin L <cheyinl@gmail.com>
2021-08-10 02:37:53 +0800

    buffer read chunck untill buffer filled or timeout

--- contents/httpcontent/readchunk.go
+++ contents/httpcontent/readchunk.go
@@ -3,27 +3,38 @@ package httpcontent
 import (
 	"io"
 	"log"
+	"time"
 )
 
+const maxChunkReadDelay = time.Millisecond * 100
+
 func readBytesChunk(fullBuf []byte, bodyReader io.ReadCloser) (loadedBuf []byte, completed bool, err error) {
-	loadedBuf = fullBuf
-	var n int
-	if n, err = bodyReader.Read(loadedBuf); nil != err {
-		if err == io.EOF {
-			completed = true
-			if n <= 0 {
-				loadedBuf = nil
+	remain := len(fullBuf)
+	offset := 0
+	startAt := time.Now()
+	for (remain > 0) && (time.Since(startAt) < maxChunkReadDelay) {
+		var n int
+		if n, err = bodyReader.Read(fullBuf[offset:]); nil != err {
+			if err == io.EOF {
+				completed = true
+				if n > 0 {
+					offset += n
+				}
+				if offset > 0 {
+					loadedBuf = fullBuf[:offset]
+				}
+				err = nil
 			} else {
-				loadedBuf = loadedBuf[:n]
+				log.Printf("ERROR: (readBytesChunk) load request failed: %v", err)
 			}
-			err = nil
-		} else {
-			log.Printf("ERROR: (readBytesChunk) load request failed: %v", err)
+			return
+		} else if n > 0 {
+			offset += n
+			remain -= n
 		}
-	} else if n > 0 {
-		loadedBuf = loadedBuf[:n]
-	} else {
-		loadedBuf = nil
+	}
+	if offset > 0 {
+		loadedBuf = fullBuf[:offset]
 	}
 	return
 }
