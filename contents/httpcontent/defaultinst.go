bc4c0d943fdc5b8ad8617198ee090f064bdd1f10
Cheyin L <cheyinl@gmail.com>
2021-09-18 04:15:36 +0800

    do not resolve local redirects

--- contents/httpcontent/defaultinst.go
+++ contents/httpcontent/defaultinst.go
@@ -2,6 +2,8 @@ package httpcontent
 
 import (
 	"crypto/tls"
+	"errors"
+	"log"
 	"net/http"
 	"time"
 
@@ -24,6 +26,31 @@ func init() {
 		InsecureSkipVerify: true,
 	}
 	httpDefaultClient = &http.Client{
-		Transport: transportInst,
+		CheckRedirect: checkRedirect,
+		Transport:     transportInst,
 	}
+}
+
+func findRequestTargetHost(r *http.Request) string {
+	if r.Host != "" {
+		return r.Host
+	}
+	return r.URL.Host
+}
+
+func checkRedirect(req *http.Request, via []*http.Request) error {
+	viaSize := len(via)
+	if viaSize >= 10 {
+		return errors.New("stopped after 10 redirects")
+	}
+	if viaSize > 1 {
+		lastReq := via[viaSize-1]
+		log.Printf("TRACE: (checkRedirect) redirect from [%s](%d) to [%s].", lastReq.URL.String(), viaSize, req.URL.String())
+		if findRequestTargetHost(req) == findRequestTargetHost(lastReq) {
+			return http.ErrUseLastResponse
+		}
+	} else {
+		log.Printf("TRACE: (checkRedirect) redirect to [%s] with empty via.", req.URL.String())
+	}
+	return nil
 }
